7bedc01457180dda0b72f5a9acd32366473740aa
Rostyslav Synenko <rostyslav.synenko@gmail.com>
2021-01-13 17:01:09 +0200

    Bolt for JavaScript app

---
+++ app.js
@@ -0,0 +1,43 @@
+const { App } = require('@slack/bolt');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/config.env' });
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+app.message(/hello/i, async ({ message, say }) => {
+  await say({
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Hey there <@${message.user}>!`
+        },
+        accessory: {
+          type: 'button',
+          text: {
+            type: 'plain_text',
+            text: 'Click Me'
+          },
+          action_id: 'button_click'
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>!`
+  });
+});
+
+app.action('button_click', async ({ body, ack, say }) => {
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
